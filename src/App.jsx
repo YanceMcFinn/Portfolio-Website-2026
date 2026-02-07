7ebc779ebc428fa14f6c5060375079e303464f5c
Sean Cole <seancole@MacBook-Air.local>
2026-02-06 22:32:49 -0500

    Fix Navbar filename casing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import NavBar from "./components/Navbar"
+import NavBar from "./components/NavBar"
 import Gallery from "./components/Gallery"
 import ProjectModal from "./components/ProjectModal"
 import projects from "./data/projects"
